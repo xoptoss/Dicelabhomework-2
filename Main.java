e9fc434bc9703f14ff9c89e548bf62bf33c42454
James Perez <xoptoss@gmail.com>
2014-06-01 21:38:31 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+package yammy.main;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main{
+
+	public static void main(String[] args) {
+
+		DiceRoller[] Roller = new DiceRoller[25];
+
+		for(int i = 0; i<Roller.length;i++)
+		{
+			//create task with array
+			Roller[i]= new DiceRoller("Task"+i);	
+		}
+		
+		System.out.println("Starting executor");
+		ExecutorService TE = Executors.newCachedThreadPool();
+		
+		for(int i = 0;i<Roller.length;i++){
+			//execute with array
+			TE.execute (Roller[i]);
+		}	
+		TE.shutdown();
+		System.out.println("Task started, main ended.\n");
+
+	}
+}
